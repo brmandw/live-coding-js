42e057ecb28d1139a56415508c7d40520a1b374e
Amanda NR <amndofc@gmail.com>
2025-09-04 20:14:14 +0700

    30 menit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let array = [8, 3, 9, 2, 5]
 
 function cek(input, arr){
-    for(let i = 0; i < arr.length; ){
+    for(let i = 0; i < arr.length; i++ ){
         nomor = arr[i] // ambil semua array
     }
 
