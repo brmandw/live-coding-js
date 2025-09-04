825eafcb1cda14b43209e7da8464ae082d4fd703
Amanda NR <amndofc@gmail.com>
2025-09-04 20:24:15 +0700

    40 menit

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ let array = [8, 3, 9, 2, 5]
 function cek(input, arr){
     let nomor = 0
     for(let i = 0; i < arr.length; i++ ){
-        nomor = arr[i] // ambil semua array
+        nomor = arr[i] 
     }
-
-    console.log(nomor)
 
     if (input === nomor){ 
         return input
