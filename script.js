babc70149ddf16ab90489b220cc13c949037ca08
Amanda NR <amndofc@gmail.com>
2025-09-04 20:04:31 +0700

    20 menit

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let array = [8, 3, 9, 2, 5]
 
 function cek(input, arr){
-    for(let i = 0; i < arr.length; i++){
-        nomor = arr[i]
+    for(let i = 0; i < arr.length; ){
+        nomor = arr[i] // ambil semua array
     }
-    console.log(nomor)
-    if (input === nomor){
+
+    if (input === nomor){ 
         return input
     } else if (isNaN(input)){
         return "inputan harus angka"
     } else{
-        return true
+        return false
     }
 }
 
